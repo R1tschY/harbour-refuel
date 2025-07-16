02635c002a916f9202efcb8176ebf888ad3b5805
Richard Liebscher <r1tschy@posteo.de>
2025-07-16 18:03:56 +0200

    Update translations

--- translations/harbour-refuel-de.ts
+++ translations/harbour-refuel-de.ts
@@ -1,37 +1,334 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+<context>
+    <name></name>
+    <message id="g95-e5">
+        <source>Unleaded E5</source>
+        <extracomment>Gasoline with 95 RON and 5 percent ethanol</extracomment>
+        <translation type="vanished">Super E5</translation>
+    </message>
+    <message id="g95">
+        <source>Unleaded</source>
+        <extracomment>Gasoline with 95 RON and standard ethanol fraction</extracomment>
+        <translation type="vanished">Super</translation>
+    </message>
+    <message id="g98">
+        <source>Unleaded Premium</source>
+        <extracomment>Gasoline with 98 RON</extracomment>
+        <translation type="vanished">Super Plus</translation>
+    </message>
+    <message id="d">
+        <source>Diesel</source>
+        <translation type="vanished">Diesel</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Refuel</source>
+        <translation type="unfinished">Über Refuel</translation>
+    </message>
+    <message>
+        <source>Search for fuel station prices</source>
+        <translation type="unfinished">Suche nach Tankstellenpreisen</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation type="unfinished">Version %1</translation>
+    </message>
+    <message>
+        <source>Copyright 2023 Richard Liebscher</source>
+        <translation type="unfinished">Copyright 2023 Richard Liebscher</translation>
+    </message>
+    <message>
+        <source>Visit on GitHub</source>
+        <translation type="unfinished">Besuche auf GitHub</translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation type="unfinished">Lizenz</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="unfinished">Credits</translation>
+    </message>
+    <message>
+        <source>
+&lt;h4&gt;Map data&lt;/h4&gt;
+is licensed under the &lt;a href=&apos;https://opendatacommons.org/licenses/odbl/&apos;&gt;Open Data Commons Open Database License (ODbL)&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Copyright &lt;a href=&apos;https://www.openstreetmap.org/copyright&apos;&gt;OpenStreetMap&lt;/a&gt; contributors&lt;br/&gt;
+
+&lt;h4&gt;Geocoding&lt;/h4&gt;
+using &lt;a href=&apos;https://photon.komoot.io/&apos;&gt;Photon&lt;/a&gt;&lt;br/&gt;
+
+&lt;h4&gt;DSEG&lt;/h4&gt;
+is licensed under the &lt;a href=&apos;http://scripts.sil.org/OFL&apos;&gt;SIL Open Font License 1.1&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Copyright 2020 &lt;a href=&apos;https://www.keshikan.net&apos;&gt;keshikan&lt;/a&gt;&lt;br/&gt;
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ChooseLocationDialog</name>
+    <message>
+        <source>Choose position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>© &lt;a href=&quot;https://www.openstreetmap.org/copyright&quot;&gt;OpenStreetMap&lt;/a&gt; contributors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nothing found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Type to start search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>CoverPage</name>
     <message>
         <source>My Cover</source>
-        <translation>Mein Cover</translation>
+        <translation type="vanished">Mein Cover</translation>
+    </message>
+</context>
+<context>
+    <name>FavsCover</name>
+    <message>
+        <source>Refuel</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>FirstPage</name>
     <message>
         <source>Show Page 2</source>
-        <translation>Zur Seite 2</translation>
+        <translation type="vanished">Zur Seite 2</translation>
     </message>
     <message>
         <source>UI Template</source>
-        <translation>UI-Vorlage</translation>
+        <translation type="vanished">UI-Vorlage</translation>
     </message>
     <message>
         <source>Hello Sailors</source>
-        <translation>Hallo Matrosen</translation>
+        <translation type="vanished">Hallo Matrosen</translation>
+    </message>
+</context>
+<context>
+    <name>FuelPriceProvider</name>
+    <message>
+        <source>Unleaded E5</source>
+        <extracomment>Gasoline with 95 RON and 5 percent ethanol</extracomment>
+        <translation type="unfinished">Super E5</translation>
+    </message>
+    <message>
+        <source>Unleaded E10</source>
+        <extracomment>Gasoline with 95 RON and 10 percent ethanol</extracomment>
+        <translation type="unfinished">Super E10</translation>
+    </message>
+    <message>
+        <source>Unleaded Premium</source>
+        <extracomment>Gasoline with 98 RON</extracomment>
+        <translation type="unfinished">Super Plus</translation>
+    </message>
+    <message>
+        <source>Diesel</source>
+        <translation type="unfinished">Diesel</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <source>Refuel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last Searches</source>
+        <translation>Letzte Suchen</translation>
+    </message>
+    <message>
+        <source>Forget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forgotten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No searches done yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to start a search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New search</source>
+        <translation>Neue Suche</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <source>Favorites</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OsmTiledMappingManagerEngine</name>
+    <message>
+        <source>Street Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OpenStreetMap Standard tile layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OpenStreetMap German tile layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Near</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search radius</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>SecondPage</name>
     <message>
         <source>Nested Page</source>
-        <translation>Unterseite</translation>
+        <translation type="vanished">Unterseite</translation>
     </message>
     <message>
         <source>Item</source>
-        <translation>Element</translation>
+        <translation type="vanished">Element</translation>
+    </message>
+</context>
+<context>
+    <name>Station</name>
+    <message>
+        <source>Wrong update data received</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StationDetailsPage</name>
+    <message>
+        <source>Prices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Opening Hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Closed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No application installed to open geo location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Public Holiday</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StationListPage</name>
+    <message>
+        <source>Stations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No station found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TankerKoenigProvider</name>
+    <message>
+        <source>Licence: CC BY 4.0 - &lt;a href=&quot;https://creativecommons.tankerkoenig.de&quot;&gt;Tankerkönig&lt;/a&gt;&lt;br&gt;Data: MTS-K</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- translations/harbour-refuel.ts
+++ translations/harbour-refuel.ts
@@ -1,36 +1,300 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en">
 <context>
-    <name>CoverPage</name>
+    <name></name>
+    <message id="g95-e5">
+        <source>Unleaded E5</source>
+        <extracomment>Gasoline with 95 RON and 5 percent ethanol</extracomment>
+        <translation type="vanished">Unleaded E5</translation>
+    </message>
+    <message id="g95">
+        <source>Unleaded</source>
+        <extracomment>Gasoline with 95 RON and standard ethanol fraction</extracomment>
+        <translation type="vanished">Unleaded</translation>
+    </message>
+    <message id="g98">
+        <source>Unleaded Premium</source>
+        <extracomment>Gasoline with 98 RON</extracomment>
+        <translation type="vanished">Unleaded Premium</translation>
+    </message>
+    <message id="d">
+        <source>Diesel</source>
+        <translation type="vanished">Diesel</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
     <message>
-        <source>My Cover</source>
+        <source>About Refuel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search for fuel station prices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright 2023 Richard Liebscher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Visit on GitHub</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Licence</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>
+&lt;h4&gt;Map data&lt;/h4&gt;
+is licensed under the &lt;a href=&apos;https://opendatacommons.org/licenses/odbl/&apos;&gt;Open Data Commons Open Database License (ODbL)&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Copyright &lt;a href=&apos;https://www.openstreetmap.org/copyright&apos;&gt;OpenStreetMap&lt;/a&gt; contributors&lt;br/&gt;
+
+&lt;h4&gt;Geocoding&lt;/h4&gt;
+using &lt;a href=&apos;https://photon.komoot.io/&apos;&gt;Photon&lt;/a&gt;&lt;br/&gt;
+
+&lt;h4&gt;DSEG&lt;/h4&gt;
+is licensed under the &lt;a href=&apos;http://scripts.sil.org/OFL&apos;&gt;SIL Open Font License 1.1&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;
+Copyright 2020 &lt;a href=&apos;https://www.keshikan.net&apos;&gt;keshikan&lt;/a&gt;&lt;br/&gt;
+</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>FirstPage</name>
+    <name>ChooseLocationDialog</name>
     <message>
-        <source>Show Page 2</source>
+        <source>Choose position</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>UI Template</source>
+        <source>Search address</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Hello Sailors</source>
+        <source>© &lt;a href=&quot;https://www.openstreetmap.org/copyright&quot;&gt;OpenStreetMap&lt;/a&gt; contributors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Nothing found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Type to start search</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
-    <name>SecondPage</name>
+    <name>FavsCover</name>
     <message>
-        <source>Nested Page</source>
+        <source>Refuel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FuelPriceProvider</name>
+    <message>
+        <source>Unleaded E5</source>
+        <extracomment>Gasoline with 95 RON and 5 percent ethanol</extracomment>
+        <translation type="unfinished">Unleaded E5</translation>
+    </message>
+    <message>
+        <source>Unleaded E10</source>
+        <extracomment>Gasoline with 95 RON and 10 percent ethanol</extracomment>
+        <translation type="unfinished">Unleaded E10</translation>
+    </message>
+    <message>
+        <source>Unleaded Premium</source>
+        <extracomment>Gasoline with 98 RON</extracomment>
+        <translation type="unfinished">Unleaded Premium</translation>
+    </message>
+    <message>
+        <source>Diesel</source>
+        <translation type="unfinished">Diesel</translation>
+    </message>
+</context>
+<context>
+    <name>HomePage</name>
+    <message>
+        <source>Refuel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Item</source>
+        <source>Last Searches</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forgotten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No searches done yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pull down to start a search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Favorites</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OsmTiledMappingManagerEngine</name>
+    <message>
+        <source>Street Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OpenStreetMap Standard tile layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OpenStreetMap German tile layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Near</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fuel type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Station</name>
+    <message>
+        <source>Wrong update data received</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StationDetailsPage</name>
+    <message>
+        <source>Prices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Opening Hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Closed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No application installed to open geo location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Monday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Friday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tuesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wednesday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thursday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Saturday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sunday</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Public Holiday</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StationListPage</name>
+    <message>
+        <source>Stations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No station found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TankerKoenigProvider</name>
+    <message>
+        <source>Licence: CC BY 4.0 - &lt;a href=&quot;https://creativecommons.tankerkoenig.de&quot;&gt;Tankerkönig&lt;/a&gt;&lt;br&gt;Data: MTS-K</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
